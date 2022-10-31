db7e879873d13567145b0962636677ae7d30c5cd
Adrián Acosa Sánchez <adri110301@gmail.com>
2022-10-31 11:04:20 +0100

    Avanza #26 ya que se ha representado un electrodoméstico

---
+++ internal/appliance.go
@@ -0,0 +1,17 @@
+package appliance
+
+type Appliance struct {
+	Brand string
+	Model string
+	Consumption int
+	Category string
+}
+
+func NewAppliance(brand string, model string, consumption int, category string) Appliance {
+	return Appliance {
+		Brand: brand,
+		Model: model,
+		Consumption: consumption,
+		Category: category,
+	}
+}
