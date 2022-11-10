1ab5f5a736feccf6b1eac096cba08ebfda28bb98
Adrián Acosa Sánchez <adri110301@gmail.com>
2022-11-10 20:10:48 +0100

    :construction:#30,#31,#32: modela appliances.go
    Borra users.go y modela appliances.go con lo hablado en los issues
    mencionados.

--- internal/appliance.go
+++ internal/appliance.go
@@ -1,15 +1,7 @@
 package internal
 
 type Appliance struct {
-	Name string
-	Consumption float32 // expressed in Wh
-	Duration int // expressed in minutes
-}
-
-func NewAppliance(name string, consumption float32, duration int) Appliance {
-	return Appliance {
-		Name: name,
-		Consumption: consumption,
-		Duration: duration,
-	}
+	Id string
+	Consumption int
+	UsageTime  int
 }
