1d0383c394c04007acebfa6e9f49ea9c314c6ae8
luistf24 <luis.tormo24@gmail.com>
2022-11-26 12:38:38 +0100

    fix: Los test tienen que empezar por Test #46

--- internal/light_info_test.go
+++ internal/light_info_test.go
@@ -5,13 +5,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func isEmpty(t *testing.T) {
+func TestIsEmpty(t *testing.T) {
 	prueba := new(LightInfo);
 
 	assert.Empty(t, prueba.generate(), "La obtención de datos de la API ha fallado")
 }
 
-func size(t *testing.T) {
+func TestSize(t *testing.T) {
 	prueba := new(LightInfo);
 
 	assert.Equal(t, 24, prueba.generate(), "El número de elementos almacenados de la API tiene que ser 24")
