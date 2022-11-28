2fe57c37b2c8a5872f86be12e083b238bec777b1
luistf24 <luis.tormo24@gmail.com>
2022-11-28 10:04:42 +0100

    Añado test para obtener datos de la API #46
    Con el desarrollo de estos estes ya se podría desarrollar el código para #43
    y #44

--- internal/light_info_test.go
+++ internal/light_info_test.go
@@ -2,13 +2,37 @@ package internal
 
 import (
 	"testing"
+	"net/http"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestGetApi(t *testing.T) {
+	response, err := http.Get("https://api.preciodelaluz.org/v1/prices/all?zone=PCB")
+
+	var mensaje string
+	var test bool
+
+	test = true
+
+	if err != nil {
+		mensaje = "Error al establecer la conexión con la API"
+		test = false
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		mensaje = "Error al obtener los datos de la API"
+		test = false
+	}
+
+	assert.Equal(t, true, test, mensaje)
+}
 
 func TestIsEmpty(t *testing.T) {
 	prueba := new(LightInfo);
 
-	assert.Empty(t, prueba.generate(), "La obtención de datos de la API ha fallado")
+	assert.NotEqual(t, len(prueba.generate()), 0, "No hay datos almacenados de la API")
 }
 
 func TestSize(t *testing.T) {
@@ -16,3 +40,4 @@ func TestSize(t *testing.T) {
 
 	assert.Equal(t, 24, prueba.generate(), "El número de elementos almacenados de la API tiene que ser 24")
 }
+
