ab390503c7b6c7be374a479e05f312cf5de1c86e
luistf24 <luis.tormo24@gmail.com>
2022-11-28 20:53:12 +0100

    fix: Corrijo algunos errores en los test #46

--- internal/light_info_test.go
+++ internal/light_info_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"net/http"
 	"github.com/stretchr/testify/assert"
+	"fmt"
 )
 
 func TestGetApi(t *testing.T) {
@@ -30,14 +31,13 @@ func TestGetApi(t *testing.T) {
 }
 
 func TestIsEmpty(t *testing.T) {
-	prueba := new(LightInfo);
 
-	assert.NotEqual(t, len(prueba.generate()), 0, "No hay datos almacenados de la API")
+	fmt.Print(generate())
+	assert.NotEqual(t, len(generate().brackets), 0, "No hay datos almacenados de la API")
 }
 
 func TestSize(t *testing.T) {
-	prueba := new(LightInfo);
 
-	assert.Equal(t, 24, prueba.generate(), "El número de elementos almacenados de la API tiene que ser 24")
+	assert.Equal(t, 24, len(generate().brackets), "El número de elementos almacenados de la API tiene que ser 24")
 }
 
