891b7e796a8aade962dbf8da47517fedb6b45a45
luistf24 <luis.tormo24@gmail.com>
2022-12-19 11:26:17 +0100

    feat: Añado más test #48

--- inferencia/inferencia_test.go
+++ inferencia/inferencia_test.go
@@ -7,6 +7,8 @@ import (
 )
 
 
+var prueba [5]float32
+
 func TestCargaInternal(t *testing.T) {
 	var test = internal.Appliance{Id:"Prueba", Consumption:1, UsageTime:1}
 	assert.Equal(t, test.Id, "Prueba", "No se ha cargado correctamente el paquete internal del proyecto")
@@ -17,11 +19,30 @@ func TestCalcReq(t *testing.T) {
 	var x []float32
 	var y []float32
 
-	_, err := calcReq(x,y)
-
+	temp, err := calcReq(x,y)
+	prueba = temp
 	if err != nil {
 		t.Fatal(err)
 	}
 }
 
+
+func TestMediaX(t *testing.T) {
+	assert.LessOrEqual(t, 0, prueba[0], "La media de números positivos tiene que ser mayor que 0")
+}
+
+
+func TestMediaY(t *testing.T) {
+	assert.LessOrEqual(t, 0, prueba[1], "La media de números positivos tiene que ser mayor que 0")
+}
+
+
+func TestVarianzaX(t *testing.T) {
+	assert.LessOrEqual(t, 0, prueba[2], "La varianza es siempre positiva")
+}
+
+
+func TestVarianzaY(t *testing.T){
+	assert.LessOrEqual(t, 0, prueba[3], "La varianza es siempre postiva")
+}
 
