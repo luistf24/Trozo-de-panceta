0ff2f50874c003c3e77c7ee5f5a80f9ff4d44783
luistf24 <luis.tormo24@gmail.com>
2022-12-19 12:31:46 +0100

    feat: Añado implementación de calcReq #48

--- inferencia/inferencia.go
+++ inferencia/inferencia.go
@@ -1,7 +1,56 @@
 package inferencia
 
+import (
+	"math"
+)
 
+/**
+ * @return: Array con 5 elementos donde se almacena:
+ *	salida[0] -> Media de X
+ *	salida[1] -> Media de Y
+ *	salida[2] -> Varianza de X
+ *	salida[3] -> Varianza de Y
+ *	salida[4] -> Covarianza de X y de Y
+ */
 func calcReq(x []float32, y[]float32)([5]float32, error) {
 	var salida [5]float32
-	return salida,nil
+	tamX := len(x)
+	tamY := len(y)
+
+	if(tamX != tamY) {
+		return salida, &errorCalcReq{"los tamaños de x e y tienen que ser iguales"}
+	}
+
+	// Cálculamos las medias de X e Y
+	var tempx float32
+	var tempy float32
+	tempx = 0
+	tempy = 0
+	for i := 0; i < tamX; i++ {
+		tempx += x[i]
+		tempy += y[i]
+    }
+	salida[0] = tempx/float32(tamX)
+	salida[1] = tempy/float32(tamX)
+
+	// Cálculamos las varianzas de X e Y
+	var powx float64
+	var powy float64
+	powx = 0
+	powy = 0
+	for i := 0; i < tamX; i++ {
+		powx += math.Pow(float64(x[i]-salida[0]), 2)
+		powy += math.Pow(float64(y[i]-salida[1]), 2)
+    }
+	salida[2] = float32(powx)/float32(tamX)
+	salida[3] = float32(powy)/float32(tamX)
+
+	// Cálculamos la covarianza
+	tempx = 0
+	for i := 0; i < tamX; i++ {
+		tempx +=(x[i]-salida[0])*(y[i]-salida[1])
+    }
+	salida[4] = tempx/float32(tamX)
+
+	return salida, nil
 }
