9819d98b27a1637db5c36b31788b925e199fd0e1
luistf24 <luis.tormo24@gmail.com>
2022-12-19 12:58:04 +0100

    feat: Añado implementación de calcRR closes #48

--- inferencia/inferencia.go
+++ inferencia/inferencia.go
@@ -29,7 +29,7 @@ func calcReq(x []float32, y[]float32)([5]float32, error) {
 	for i := 0; i < tamX; i++ {
 		tempx += x[i]
 		tempy += y[i]
-    }
+	}
 	salida[0] = tempx/float32(tamX)
 	salida[1] = tempy/float32(tamX)
 
@@ -41,7 +41,7 @@ func calcReq(x []float32, y[]float32)([5]float32, error) {
 	for i := 0; i < tamX; i++ {
 		powx += math.Pow(float64(x[i]-salida[0]), 2)
 		powy += math.Pow(float64(y[i]-salida[1]), 2)
-    }
+	}
 	salida[2] = float32(powx)/float32(tamX)
 	salida[3] = float32(powy)/float32(tamX)
 
@@ -49,8 +49,22 @@ func calcReq(x []float32, y[]float32)([5]float32, error) {
 	tempx = 0
 	for i := 0; i < tamX; i++ {
 		tempx +=(x[i]-salida[0])*(y[i]-salida[1])
-    }
+	}
 	salida[4] = tempx/float32(tamX)
 
 	return salida, nil
+}
+
+
+
+func calcRR(x []float32, y[]float32)(func(int)float32, error) {
+	datos, err := calcReq(x,y)
+
+	if err != nil {
+		return nil, &errorCalcRR{" erro calculando los datos necesarios usando la función calcReq"}
+	}
+
+	return func(t int) float32 {
+        return datos[4]*(float32(t)-datos[0])/datos[2]+datos[1]
+    }, nil
 }
