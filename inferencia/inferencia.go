aa989425307c58235bd27a7d3c17bc709343b37d
luistf24 <luis.tormo24@gmail.com>
2022-12-19 10:00:10 +0100

    feat: Se crea test para la carga de internal #49
    Se crea test para comprobrar que el paquete internal se carga correctamente

--- inferencia/inferencia.go
+++ inferencia/inferencia.go
@@ -1,5 +1,2 @@
 package inferencia
 
-import (
-	"Trozo-de-panceta/internal"
-)
--- inferencia/inferencia_test.go
+++ inferencia/inferencia_test.go
@@ -1,2 +1,15 @@
 package inferencia
 
+import (
+	"Trozo-de-panceta/internal"
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+
+func TestCargaInternal(t *testing.T) {
+	var test = internal.Appliance{Id:"Prueba", Consumption:1, UsageTime:1}
+	assert.Equal(t, test.Id, "Prueba", "No se ha cargado correctamente el paquete internal del proyecto")
+}
+
+
