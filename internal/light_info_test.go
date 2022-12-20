d998d9b75517556db836c94506c9a4fb71c14c25
luistf24 <luis.tormo24@gmail.com>
2022-12-20 10:16:14 +0100

    feat: Añado fixture en el test de LightInfo #46

--- internal/light_info_test.go
+++ internal/light_info_test.go
@@ -6,29 +6,42 @@ import (
 )
 
 
-var temp LightInfo
+type LightInfoFixture struct {
+	temp LightInfo
+	err error
+}
+
+func NewFixture() LightInfoFixture {
+	var salida = new (LightInfoFixture)
+
+	_, salida.err = salida.temp.generate("../data/test.json")
+
+	return *salida
+}
+
+var fixture = NewFixture()
+
 
 func TestErrorLightInfo(t *testing.T) {
-	_, err := temp.generate("../data/test.json")
 
-	if err != nil {
-		t.Fatal(err)
+	if fixture.err != nil {
+		t.Fatal(fixture.err)
 	}
 }
 
 
 func TestIsEmpty(t *testing.T) {
-	assert.NotEqual(t, len(temp.brackets), 0, "No hay datos almacenados de la API")
+	assert.NotEqual(t, len(fixture.temp.brackets), 0, "No hay datos almacenados de la API")
 }
 
 
 func TestSize(t *testing.T) {
-	assert.Equal(t, 24, len(temp.brackets), "El número de elementos almacenados de la API tiene que ser 24")
+	assert.Equal(t, 24, len(fixture.temp.brackets), "El número de elementos almacenados de la API tiene que ser 24")
 }
 
 
 func TestGenerateTimeBracketTime(t * testing.T) {
-	time, err := temp.generateTimeBracket(0)
+	time, err := fixture.temp.generateTimeBracket(0)
 
 	if err != nil {
 		t.Fatal(err)
